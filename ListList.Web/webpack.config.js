45c06365aa7e22cb32b99c9b2d6324e4fcfab67f
Michael <turkeymaster77@gmail.com>
2025-08-08 08:21:56 -0400

    Load config from environment vars

--- ListList.Web/webpack.config.js
+++ ListList.Web/webpack.config.js
@@ -1,71 +1,85 @@
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: isProd ? '[name].[contenthash].js' : 'bundle.js',
-    clean: true,
-  },
-  mode: isProd ? 'production' : 'development',
-  devtool: isProd ? false : 'source-map',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.module\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]__[hash:base64:5]',
+module.exports = (env, argv) => {
+  const envFile = `config/.env.${argv.mode || 'development'}`;
+  const envVars = dotenv.config({ path: envFile }).parsed || {};
+
+  const defineEnv = Object.entries(envVars).reduce((acc, [key, val]) => {
+    acc[`process.env.${key}`] = JSON.stringify(val);
+
+    return acc;
+  }, {});
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProd ? '[name].[contenthash].js' : 'bundle.js',
+      clean: true,
+    },
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd ? false : 'source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.module\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]__[local]__[hash:base64:5]',
+                },
+                esModule: true,
               },
-              esModule: true,
             },
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.scss$/,
-        exclude: /\.module\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset',
-      },
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.scss$/,
+          exclude: /\.module\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset',
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin(defineEnv),
+      new HtmlWebpackPlugin({
+        template: './src/public/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: isProd ? '[name].[contenthash].css' : '[name].css',
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/public/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: isProd ? '[name].[contenthash].css' : '[name].css',
-    }),
-  ],
-  devServer: {
-    static: './dist',
-    hot: true,
-    historyApiFallback: true,
-    port: 9000,
-  },
+    devServer: {
+      static: './dist',
+      hot: true,
+      historyApiFallback: true,
+      port: 9000,
+    },
+  };
 };
